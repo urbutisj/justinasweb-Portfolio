0049c65403b117363131619d801343f83632109e
justinas <urbutisj@gmail.com>
2022-03-23 23:13:04 +0200

    Fixed Minor issues + finished Footer

--- frontend_portfolio/src/wrapper/MotionWrap.js
+++ frontend_portfolio/src/wrapper/MotionWrap.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-const MotionWrap = (Component, className) =>
+const MotionWrap = (Component, classNames) =>
   function HOC() {
     return (
       <motion.div
         whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
         transition={{ duration: 0.5 }}
-        className="{`${className} app__flex`}"
+        className={`${classNames} app__flex`}
       >
         <Component />
       </motion.div>
