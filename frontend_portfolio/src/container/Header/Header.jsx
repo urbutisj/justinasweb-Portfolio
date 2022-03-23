1d0180bff18a82ca15e9e62bc1eb4ec7a671cf7a
justinas <urbutisj@gmail.com>
2022-03-23 11:09:49 +0200

    Higher-Order Components

--- frontend_portfolio/src/container/Header/Header.jsx
+++ frontend_portfolio/src/container/Header/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { motion } from "framer-motion";
 
+import { AppWrap } from "../../wrapper";
 import { images } from "../../constants";
 
 import "./Header.scss";
@@ -19,7 +20,7 @@ const scaleVariants = {
 
 function Header() {
   return (
-    <div id="home" className="app__header app__flex">
+    <div className="app__header app__flex">
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 0.5 }}
@@ -69,4 +70,4 @@ function Header() {
   );
 }
 
-export default Header;
+export default AppWrap(Header, "home");
